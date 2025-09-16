f161542704a4e63d58067fda4f113e1ed4ab9de8
Students <Students@310PC12>
2025-09-16 16:51:09 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+amespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Woadad!");
+        
+        }
+    }
+}
